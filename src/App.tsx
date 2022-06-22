b915d97af8ea7662a39ed43f53493253c9029966
PedroCantanhede <pedroh.cantanhede@hotmail.com>
2022-06-22 15:35:08 -0300

    :date: Structuring the Header, Sidebar and Consuming GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { useEffect } from "react"
-import { gql, useQuery } from "@apollo/client"
+import { Event } from './pages/Event';
 
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
-  )
-
-  return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello, World!</h1>
+    <div>
+      <Event />
+    </div>
   )
 }
 
